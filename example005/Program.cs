28dfe172d75caef6e6cc3ef75f200e05d92eb05b
vladya <vladyslav.davydov100@gmail.com>
2023-01-13 22:33:40 +0300

    example 005

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("insert name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Kiska")
+{
+    Console.WriteLine("HEY ITS KISKA");
+}
+else
+{
+    Console.Write("Shalom BISHES ");
+    Console.WriteLine(username);
+}
